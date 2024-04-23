7bc3c49c74c5c5e949223a3ac7e6e021eea987ed
Aboyeji Emmanuel <aboyejiemmanuel07@gmail.com>
2024-04-23 23:01:28 +0100

    search functionality

--- app/search.tsx
+++ app/search.tsx
@@ -1,7 +1,38 @@
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
+import { useContext } from "react";
+import { Text } from "react-native-paper";
+
+import { StoreContext } from "./_layout";
+import { StockCard } from "@/components/StockCard";
 
 export default function SearchScreen() {
-  return <View style={styles.container}></View>;
+  const { searchQuery, searchedStocks } = useContext(StoreContext);
+
+  if (!searchQuery && searchedStocks.length === 0)
+    return (
+      <View style={styles.container}>
+        <Text variant="titleLarge" style={{ fontWeight: "bold" }}>
+          Search Stocks
+        </Text>
+      </View>
+    );
+
+  if (searchQuery && searchedStocks.length === 0)
+    return (
+      <View style={styles.container}>
+        <Text variant="titleLarge" style={{ fontWeight: "bold" }}>
+          No Stocks Found :(
+        </Text>
+      </View>
+    );
+
+  return (
+    <FlatList
+      data={searchedStocks}
+      keyExtractor={(item) => item.ticker}
+      renderItem={({ item }) => <StockCard {...item} />}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
