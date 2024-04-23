14d53cc8b7b5e8316355c810630064242b177c14
Aboyeji Emmanuel <aboyejiemmanuel07@gmail.com>
2024-04-24 00:08:31 +0100

    watchlist tab

--- app/(tabs)/watchlist.tsx
+++ app/(tabs)/watchlist.tsx
@@ -1,7 +1,82 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, FlatList, Animated, Pressable } from "react-native";
+import { useContext } from "react";
+import { Text } from "react-native-paper";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+
+import { StoreContext } from "../_layout";
+import { stocks } from "@/data";
+import { StockCard } from "@/components/StockCard";
+
+function RightActions({
+  progress,
+  dragX,
+  onPress,
+}: {
+  progress: Animated.AnimatedInterpolation<string | number>;
+  dragX: Animated.AnimatedInterpolation<string | number>;
+  onPress: () => void;
+}) {
+  const scale = dragX.interpolate({
+    inputRange: [-100, 0],
+    outputRange: [0.7, 0],
+  });
+
+  return (
+    <Pressable
+      style={{
+        backgroundColor: "red",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 90,
+      }}
+      onPress={onPress}
+    >
+      <Animated.Text
+        style={{
+          fontWeight: "bold",
+          fontSize: 22,
+          color: "white",
+          transform: [{ scale }],
+        }}
+      >
+        Delete
+      </Animated.Text>
+    </Pressable>
+  );
+}
 
 export default function WatchlistScreen() {
-  return <View style={styles.container}></View>;
+  const { likedStocks, updateLikedStocks } = useContext(StoreContext);
+
+  if (likedStocks.length > 0)
+    return (
+      <View style={{ flex: 1 }}>
+        <FlatList
+          keyExtractor={(item) => item.ticker}
+          data={stocks.filter((i) => likedStocks.includes(i.ticker))}
+          renderItem={({ item }) => (
+            <Swipeable
+              renderRightActions={(progress, dragX) => (
+                <RightActions
+                  progress={progress}
+                  dragX={dragX}
+                  onPress={() => updateLikedStocks(item.ticker, "del")}
+                />
+              )}
+            >
+              <StockCard {...item} />
+            </Swipeable>
+          )}
+        />
+      </View>
+    );
+  return (
+    <View style={styles.container}>
+      <Text variant="titleLarge" style={{ fontWeight: "bold" }}>
+        No Stocks On Watchlist
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
